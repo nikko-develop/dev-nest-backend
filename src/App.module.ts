0d8c4c8ff53c9c3f163b4cc4703017154ef3059c
Nikolay.Kovalev <nikolay.kovalev@GWGNX52XXW-X5.local>
2024-09-23 03:05:18 +0300

    feat(DN-xx): Настройка линтеров и инициализация Nest

---
+++ src/App.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { HealthcheckController } from './HealtChecker.controller';
+
+const httpControllers = [HealthcheckController];
+
+@Module({
+	imports: [],
+	controllers: [...httpControllers]
+})
+export class AppModule {}
